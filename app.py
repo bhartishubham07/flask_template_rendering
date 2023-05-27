a28e5c16ff0396643d8524022402117f682f54bb
Shubham <93003194+crystalshubham@users.noreply.github.com>
2023-05-27 09:01:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+FAI = Flask(__name__)
+
+@FAI.route('/first')
+def first():
+    return render_template('first.html')
+
+@FAI.route('/data_render')
+def data_render():
+    return render_template('data_render.html', name='Shubham', age=22)
+
+
+if __name__ == '__main__':
+    FAI.run(debug=True)
